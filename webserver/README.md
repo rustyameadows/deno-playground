282365f87cf6e12d3dfe17b9776d31e2563e4159
Rusty Meadows <rustyameadows@gmail.com>
2022-07-03 23:45:46 +0000

    initial example based on - https://github.com/nanojsx/nano-jsx-deno-example

---
+++ webserver/README.md
@@ -0,0 +1,3 @@
+Web server using `oak` and `nano_jsx`.
+
+based on - https://github.com/nanojsx/nano-jsx-deno-example
---
+++ webserver/deps.ts
@@ -0,0 +1,3 @@
+export { h, renderSSR, Helmet, Component } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
+export type { FC } from 'https://deno.land/x/nano_jsx@v0.0.33/mod.ts'
+export { Router, Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts'
