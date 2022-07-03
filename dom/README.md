e74ac77301972005c879f034d02888aeff411b1e
Rusty Meadows <rustyameadows@gmail.com>
2022-07-03 01:52:39 +0000

    create basic web scraper using puppeteer and a DOM parser

---
+++ dom/README.md
@@ -0,0 +1,6 @@
+Resources:
+
+https://scrapingant.com/blog/deno-web-scraping
+
+have to run this first:\
+`PUPPETEER_PRODUCT=chrome deno run -A --unstable https://deno.land/x/puppeteer@9.0.0/install.ts`
---
+++ dom/start.tsx
@@ -0,0 +1,37 @@
+import puppeteer from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { assert } from "https://deno.land/std@0.146.0/testing/asserts.ts";
+
+// assigning url to the first argument passed to Deno CLI
+const url = Deno.args[0];
+
+try {
+  // launch browser
+  const browser = await puppeteer.launch();
+  // launch page
+  const page = await browser.newPage();
+  // navigate to url
+  await page.goto(url);
+
+  // assign url HTML content to html
+  const html = await page.content();
+
+  // parse html to DOM structure
+  const document: any = new DOMParser().parseFromString(html, "text/html");
+
+  // select h1 from the new DOM structure
+  const pageHeader = document.querySelector("h1");
+
+  // check to make sure an h1 was found
+  if (pageHeader) {
+    // return the page header's text content
+    console.log(pageHeader.textContent);
+  } else {
+    // no h1 found
+    console.log("no h1 found");
+  }
+} catch (error) {
+  console.log(error);
+}
+
+Deno.exit(1);
