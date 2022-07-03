21f7daccabf22d49e00a79e7d4813d5dfd214e8e
Rusty Meadows <rustyameadows@gmail.com>
2022-07-03 02:59:07 +0000

    basic CLI for pagespeed

--- pagespeed/README.md
+++ pagespeed/README.md
@@ -11,3 +11,13 @@ export { parse, Args } from "https://deno.land/std/flags/mod.ts";
 
 Not sure I need this library right now. Going to try and use the google API
 directly via fetch
+
+Next solution:
+
+https://developers.google.com/speed/docs/insights/v5/get-started
+
+Based on this example:
+https://developers.google.com/speed/docs/insights/v5/get-started#javascript
+
+Helpful info -
+https://gist.github.com/alekseykulikov/b5b88e4f8495c39cdcf39bc05d6b50c7
--- pagespeed/start.tsx
+++ pagespeed/start.tsx
@@ -1,9 +1,52 @@
-import { runPagespeed } from "https://deno.land/x/pagespeed/mod.ts";
+// deno run -A start.tsx https://rustyameadows.com
 
-const data = await runPagespeed("https://developers.google.com", {
-  strategy: "mobile",
-});
+const suppliedUrl = Deno.args[0];
 
-console.log(data);
+function setUpQuery() {
+  const api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
+
+  const parameters = {
+    url: encodeURIComponent(suppliedUrl),
+  };
+
+  let query = `${api}?`;
+
+  for (var key in parameters) {
+    query += `${key}=${parameters[key]}`;
+  }
+
+  query += `&category=performance&category=accessibility&category=seo`;
+
+  return query;
+}
+
+const url = setUpQuery();
+console.log(url);
+
+const jsonResponse = await fetch(url);
+
+const jsonData = await jsonResponse.json();
+
+const lighthouse = jsonData.lighthouseResult;
+
+// console.log(lighthouse.categories);
+
+const performanceScore = lighthouse.categories["performance"].score * 100;
+const accessibilityScore = lighthouse.categories["accessibility"].score * 100;
+const seoScore = lighthouse.categories["seo"].score * 100;
+
+const lighthouseMetrics = {
+  "Performance Score": performanceScore,
+  "Accessibility Score": accessibilityScore,
+  "SEO Score": seoScore,
+  "First Contentful Paint":
+    lighthouse.audits["first-contentful-paint"].displayValue,
+  "Speed Index": lighthouse.audits["speed-index"].displayValue,
+  "Time To Interactive": lighthouse.audits["interactive"].displayValue,
+  "First Meaningful Paint":
+    lighthouse.audits["first-meaningful-paint"].displayValue,
+};
+
+console.log(lighthouseMetrics);
 
 Deno.exit(1);
